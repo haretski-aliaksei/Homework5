d8af138f3a923d1a765c7d434f7fde7302d64001
haretski.aliaksei@gmail.com <haretski.aliaksei@gmail.com>
2023-03-12 21:38:55 +0100

    add checkIfSeveralHairDryerIsSelected test

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -4,29 +4,55 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class TestDemo {
     private static final String ONLINER = "https://onliner.by";
+    private static final By LOCATOR_HAIR_DRYERS = By.xpath("//span[text()='Фены']");
     private static final String DYSON = "Dyson";
+    private static final String ROWENTA = "Rowenta";
+    private static final String BABYLISS = "BaByliss";
+    private static final String BABYLISS_PRO = "BaByliss PRO";
+
+//    @Test
+//    public void checkIfHairDryerIsSelected() {
+//        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\drivers\\chromedriver_win32\\chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//        driver.get(ONLINER);
+//        driver.findElement(LOCATOR_HAIR_DRYERS).click();
+//        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 250)");
+//        driver.findElement(By.xpath("(//span[text()='" + DYSON + "'])[1]/..")).click();
+//
+//        String expectedResult = driver
+//                .findElement(By.xpath("//span[@class='schema-tags__text' and text()='Dyson']"))
+//                .getText();
+//
+//        assertThat(expectedResult).isEqualTo(DYSON);
+//
+//        driver.quit();
+//    }
 
     @Test
-    public void checkIfHairDryerIsSelected() {
-        String dyson = "Dyson";
+    public void checkIfSeveralHairDryerIsSelected() {
+        ArrayList<String> manufacturesHairDryer = new ArrayList<>();
+        manufacturesHairDryer.add(DYSON);
+        manufacturesHairDryer.add(ROWENTA);
+        manufacturesHairDryer.add(BABYLISS);
+        manufacturesHairDryer.add(BABYLISS_PRO);
 
         System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\drivers\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get(ONLINER);
-        driver.findElement(By.xpath("//span[text()='Фены']")).click();
+        driver.findElement(LOCATOR_HAIR_DRYERS).click();
         ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 250)");
-        driver.findElement(By.xpath("(//span[text()='" + DYSON + "'])[1]/..")).click();
 
-        String expectedResult = driver
-                .findElement(By.xpath("//span[@class='schema-tags__text' and text()='Dyson']"))
-                .getText();
+        driver.findElement(By.xpath("(//span[text()='" + manufacturesHairDryer.get(0) + "'])[1]/..")).click();
+        driver.findElement(By.xpath("(//span[text()='" + manufacturesHairDryer.get(1) + "'])[1]/..")).click();
+        driver.findElement(By.xpath("(//span[text()='" + manufacturesHairDryer.get(2) + "'])[1]/..")).click();
+        driver.findElement(By.xpath("(//span[text()='" + manufacturesHairDryer.get(3) + "'])[1]/..")).click();
 
-        assertThat(expectedResult).isEqualTo(DYSON);
 
-        driver.quit();
     }
 }
