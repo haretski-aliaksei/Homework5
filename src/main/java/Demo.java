c5b8ed7c2d523287089ea7ebcdaf8344b3329e72
haretski.aliaksei@gmail.com <haretski.aliaksei@gmail.com>
2023-03-12 19:18:49 +0100

    add Demo.class

---
+++ src/main/java/Demo.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\drivers\\chromedriver_win32\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://onliner.by");
+        driver.findElement(By.xpath("//span[text()='Мыши']")).click();
+        ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 250)");
+        driver.findElement(By.xpath("//input[@value='razer']/following-sibling::span")).click();
+
+        if (driver.findElement(By.xpath("//input[@value='razer']")).isSelected()) {
+            System.out.println("Still selected");
+        }
+
+        driver.quit();
+    }
+}
